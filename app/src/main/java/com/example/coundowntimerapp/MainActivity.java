860d8615e1d82127da91a374b0da279da26e1ea4
haider ali <ha347278@gmail.com>
2022-07-21 03:09:17 +0500

    toast added

--- app/src/main/java/com/example/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/coundowntimerapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.coundowntimerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         Runnable run = new Runnable() {
             @Override
             public void run() {
-                
+                Toast.makeText(MainActivity.this,"This is toast",Toast.LENGTH_SHORT);
+                handler.postDelayed(this,1000);
             }
         }
 
