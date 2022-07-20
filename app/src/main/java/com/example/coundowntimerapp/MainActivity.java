beccc22c2fb59955d0163ea522fde2ee2834f4c0
haider ali <ha347278@gmail.com>
2022-07-21 03:05:28 +0500

    handler and runnable added

--- app/src/main/java/com/example/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/coundowntimerapp/MainActivity.java
@@ -10,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                
+            }
+        }
+
     }
 }
