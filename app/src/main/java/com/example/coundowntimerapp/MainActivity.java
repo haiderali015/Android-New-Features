7fa251b551fbf6c899bcba806d507a6c6efe7b91
haider ali <ha347278@gmail.com>
2022-07-21 19:30:00 +0500

    async task class implemented

--- app/src/main/java/com/example/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/coundowntimerapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.coundowntimerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -11,12 +12,28 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class BG extends AsyncTask<String,void,String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
 
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
     }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+        }
 
     public void getview(View view)
     {
