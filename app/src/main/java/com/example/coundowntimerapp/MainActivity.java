0a84cf79d1d0fa8274b843401e64f4b04d8b9d85
haider ali <ha347278@gmail.com>
2022-07-21 18:46:43 +0500

    toast is shown on button click

--- app/src/main/java/com/example/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/example/coundowntimerapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,27 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new CountDownTimer(5000,1000)
-        {
-            @Override
-            public void onTick(long l) {
-                Log.d("TIMER","ontick : Iam Haider");
-            }
+    }
 
-            @Override
-            public void onFinish() {
-                Log.d("TIMER","khatam hogya");
-            }
-        }.start();
-
-        //        final Handler handler = new Handler();
-        //        Runnable run = new Runnable() {
-        //            @Override
-        //            public void run() {
-        //                Toast.makeText(MainActivity.this,"This is toast",Toast.LENGTH_SHORT);
-        //                handler.postDelayed(this,1000);
-        //            }
-        //        };
+    public void getview(View view)
+    {
+        Toast.makeText(this,"HI IaM TOAST",Toast.LENGTH_SHORT).show();
 
     }
 }
